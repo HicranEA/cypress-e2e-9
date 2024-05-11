694a1421b5b9f930496ca49338131ecb9e6a307d
HicranEA <hicran_er@yahoo.com>
2024-05-11 11:13:30 -0500

    cypress

---
+++ cypress/e2e/integration/05-assertions.cy.js
@@ -0,0 +1,13 @@
+describe('Assertions', () => {
+
+    beforeEach(() => {
+      cy.visit("https://www.techglobal-training.com/frontend/");
+      cy.contains(".cards", "Html Elements").click();
+    });
+  
+  
+    it('Default Assertions', () => {
+  
+    })
+  
+  })
